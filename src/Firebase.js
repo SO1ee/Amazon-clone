ab37cd655abc98634a5c8b1b8384647fa136a809
Your Name <you@example.com>
2022-11-19 13:19:39 +0530

    initil commit

---
+++ src/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBYFOHrPUvrD3p5gXFyGLccQoZCQFByrxk",
+    authDomain: "clone-643ae.firebaseapp.com",
+    projectId: "clone-643ae",
+    storageBucket: "clone-643ae.appspot.com",
+    messagingSenderId: "476672662968",
+    appId: "1:476672662968:web:c93512c547fcf47e94034d"
+  };
